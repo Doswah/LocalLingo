1394606527dddbe4a9e08f875391bf751b35aea7
Doswah <doswah@live.com>
2012-11-22 23:14:43 -1000

    Summary: Added homescreen, add_lingo, add_lingo_confirm, lingo_list activities. Created a Lingo class ( Name, Dictionary Word, Lingo Word, longitude, latitude) that stores entries.
    add_lingo:
    --------------------
    Prompts user for Name, Dictionary Word, and Lingo Word.
    Can visit Lingo List.
    
    add_lingo_list:
    --------------------
    Displays user inputs for Name, Dictionary Word, and Lingo Word.
    
    lingo_list:
    --------------------
    ListView activity that displays the Lingo objects in Lingo array.

--- gen/com/locallingo/R.java
+++ gen/com/locallingo/R.java
@@ -16,24 +16,30 @@ public final class R {
         public static final int padding_small=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int homepage=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int page_bg=0x7f020003;
     }
     public static final class id {
-        public static final int btnClose=0x7f080005;
-        public static final int btnNextScreen=0x7f080002;
-        public static final int email=0x7f080001;
-        public static final int menu_settings=0x7f080006;
-        public static final int name=0x7f080000;
-        public static final int txtEmail=0x7f080004;
-        public static final int txtName=0x7f080003;
+        public static final int btnClose=0x7f070008;
+        public static final int btnHomeAddLingo=0x7f07000a;
+        public static final int btnHomeLingoList=0x7f070009;
+        public static final int btnLingoList=0x7f070004;
+        public static final int btnNextScreen=0x7f070003;
+        public static final int dictionaryWord=0x7f070001;
+        public static final int lingoWord=0x7f070002;
+        public static final int listView1=0x7f07000b;
+        public static final int name=0x7f070000;
+        public static final int txtDictionaryWord=0x7f070006;
+        public static final int txtLingoWord=0x7f070007;
+        public static final int txtName=0x7f070005;
     }
     public static final class layout {
-        public static final int activity_home=0x7f030000;
-        public static final int activity_lingo=0x7f030001;
-    }
-    public static final class menu {
-        public static final int activity_home=0x7f070000;
+        public static final int activity_add_lingo=0x7f030000;
+        public static final int activity_add_lingo_confirm=0x7f030001;
+        public static final int activity_home=0x7f030002;
+        public static final int activity_lingo_list=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
--- src/com/locallingo/home.java
+++ src/com/locallingo/home.java
@@ -7,36 +7,32 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
- 
+
 public class home extends Activity {
-    // Initializing variables
-    EditText inputName;
-    EditText inputEmail;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
- 
-        inputName = (EditText) findViewById(R.id.name);
-        inputEmail = (EditText) findViewById(R.id.email);
-        Button btnNextScreen = (Button) findViewById(R.id.btnNextScreen);
- 
-        //Listening to button event
-        btnNextScreen.setOnClickListener(new View.OnClickListener() {
- 
-            public void onClick(View arg0) {
-                //Starting a new Intent
-                Intent nextScreen = new Intent(getApplicationContext(), lingo.class);
- 
-                //Sending data to another Activity
-                nextScreen.putExtra("name", inputName.getText().toString());
-                nextScreen.putExtra("email", inputEmail.getText().toString());
- 
-                Log.e("n", inputName.getText()+"."+ inputEmail.getText());
- 
-                startActivity(nextScreen);
- 
-            }
+        
+        Button btnHomeLingoList = (Button) findViewById(R.id.btnHomeLingoList);
+        Button btnHomeAddLingo = (Button) findViewById(R.id.btnHomeAddLingo);
+        
+        btnHomeLingoList.setOnClickListener(new View.OnClickListener() {
+        	
+        	public void onClick(View btnLingoList) {
+        		Intent lingoList = new Intent(getApplicationContext(), lingo_list.class);
+        		startActivity(lingoList);
+        	}
         });
+
+        btnHomeAddLingo.setOnClickListener(new View.OnClickListener() {
+	
+        	public void onClick(View btnLingoList) {
+        		Intent add_lingo = new Intent(getApplicationContext(), add_lingo.class);
+        		startActivity(add_lingo);
+        	}
+        });
+
     }
 }
